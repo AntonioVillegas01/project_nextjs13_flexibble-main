44c5cdaebb273bf95a41a5acb515ae4491949ee2
Marco Garcia <macbookpro@MacBook-Pro-de-Macbook.local>
2023-09-17 21:02:37 -0600

    Finish auth with google

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -10,7 +10,6 @@ type Props = {
 
 const UserProfile = async ({ params }: Props) => {
     const result = await getUserProjects(params.id, 100) as { user: UserProfile }
-
     if (!result?.user) return (
         <p className="no-result-text">Failed to fetch user info</p>
     )
